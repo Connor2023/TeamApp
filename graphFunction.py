71debcb1870dae138dfdc53876cf8e8ddc21247c
Tsag0005 <111573704+Tsag0005@users.noreply.github.com>
2023-01-31 23:47:38 +1100

    Add files via upload

---
+++ graphFunction.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+apple = yf.Ticker("AAPL")
+apple.basic_info
+apple_history = apple.history(period="2d")
+print(apple_history)
+
+def retrieve_stock_history(stock, period):
+    stock = yf.Ticker(stock)
+    stock_history = stock.history(period=period)
+    return stock_history
+
+#resolution to be decided
+def graph_stock_history(stock_history):
+    low_column = stock_history['Low'].values.tolist()
+    high_column = stock_history['High'].values.tolist()
+    avg_column = [(x+y)/2 for x, y in zip(low_column, high_column)]
+    dates = []
+    for i in range(len(stock_history.index)):
+        dates.append(stock_history.index[i].strftime("%Y-%m-%d %H:%M:%S").split()[0])
+    plt.plot(dates, avg_column)
+    plt.show()
+
+graph_stock_history(retrieve_stock_history("AAPL", "10d"))
+
+
+
+
+
